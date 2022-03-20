c48ab9bf172e7f9055861bacd08b43f24f7770c9
0Taiga0 <101995045+0Taiga0@users.noreply.github.com>
2022-03-21 00:33:15 +0300

    1

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import asyncio
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from os import getenv
+from sys import exit
 
-# Объект бота
-bot = Bot(token="5193399323:AAH5YHcvCWBHJSNFTeYhRuAuFpz2iyHn7uY")
+bot_token = getenv("BOT_TOKEN")
+if not bot_token:
+    exit("Error: no token provided")
+
+bot = Bot(token=bot_token)
 # Диспетчер для бота
 dp = Dispatcher(bot)
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -10,11 +16,11 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Хэндлер на команду /test1
-@dp.message_handler(commands="test1")
+@dp.message_handler()
 async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+    await message.answer(message.text)
 
 
 if __name__ == "__main__":
     # Запуск бота
-    executor.start_polling(dp, skip_updates=True11)
+    executor.start_polling(dp, skip_updates=True)

6ee34190ac18d3f9b6726d234c4647be0fea8aae
Sensej808 <sasha-tkachenko-03@mail.ru>
2022-03-21 23:20:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,32 +4,36 @@ from aiogram import Bot, Dispatcher, executor, types
 from os import getenv
 from sys import exit
 
-
 bot_token = getenv("BOT_TOKEN")
 if not bot_token:
     exit("Error: no token provided")
 
+
+async def end():
+    return 0
+
+
 bot = Bot(token=bot_token)
 
 dp = Dispatcher(bot)
-
+userid = 0
 chat_id = 457140523  # 463785826
-
 logging.basicConfig(level=logging.INFO)
 
 
-async def Chat(Id_admin, id_user):
-    @dp.message_handler(commands=["exit"])
-    await Exit(msg: types.Message):
-
+async def chat(userid):
+    @dp.message_handler(commands=["end"])
+    async def end():
+        return 0
 
     @dp.message_handler()
-    async def pars(msg: types.Message):
-        if msg.from_user.id == id_user:
-            await msg.bot.send_message(Id_admin, msg.text)
+    async def chat1(msg2: types.Message):
+        exit = msg2.text
+        if msg2.from_user.id == chat_id:
+            await bot.send_message(userid, "Admin" + ": " + msg2.text)
         else:
-            await msg.bot.send_message(id_user, msg.text)
-        await Chat(Id_admin, id_user)
+            await bot.send_message(chat_id, "User" + ": " + msg2.text)
+
 
 
 @dp.message_handler(commands=["start"])
@@ -41,16 +45,17 @@ async def pars(msg: types.Message):
 @dp.message_handler(commands=["send"])
 async def pars(msg: types.Message):
     await bot.send_message(chat_id, "@" + msg.from_user.username + ": " + msg.text[6:] + '\n')
+    userid = msg.from_user.id
 
     @dp.message_handler(commands=["back"])
     async def pars1(msg1: types.Message):
-        await bot.send_message(msg.from_user.id, "Admin" + ": " + msg1.text[6:])
-        await Chat(msg1.from_user.id)
+        await bot.send_message(userid, "Admin" + ": " + msg1.text[6:])
+        await chat(userid)
 
 
-@dp.message_handler()
-async def pars(msg: types.Message):
-    await msg.answer(msg.text)
+# @dp.message_handler()
+# async def pars(msg: types.Message):
+#    await msg.answer(msg.text)
 
 
 if __name__ == "__main__":

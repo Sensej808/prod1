285eb0ba62f5d1ce6556f9dc44f37e682c6860e0
0Taiga0 <101995045+0Taiga0@users.noreply.github.com>
2022-03-23 21:22:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from os import getenv
 from sys import exit
 from tokensuka import *
 
-
 bot = Bot(token=bot_token)
 userid = 0
 ban = 0
@@ -13,34 +12,43 @@ dp = Dispatcher(bot)
 chat_id = 457140523  # 463785826
 logging.basicConfig(level=logging.INFO)
 
-KB = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True)
-KB.row(aiogram.types.KeyboardButton("Связь"), aiogram.types.KeyboardButton("Связь"))
+KB = types.ReplyKeyboardMarkup(resize_keyboard=True)
+KB.row(types.KeyboardButton("Связь"), types.KeyboardButton("Связь"))
+
 
 @dp.message_handler(commands=["help", "start"])
 async def helpmes(message: types.Message):
     await message.answer("Привет! \n Команды бота:\t /help- \t /send-")
 
 
+async def sendtodaun(msg1, uid):
+    global ban
+    if msg1 == "stop":
+        ban = 0
+        return 0
+    elif uid == S:
+        await bot.send_message(userid, msg1)
+    else:
+        await bot.send_message(S, msg1)
+
+
 @dp.message_handler(commands=["send"])
 async def sendToNahoy(msg2: types.Message):
     global userid
+    global ban
     userid = msg2.from_user.id
     await bot.send_message(userid, "Устанавливаю связь с администрацией...")
     await bot.send_message(S, "Ало, ну как там с чат ботом")
-    global ban
     ban = 1
+
 
 
 @dp.message_handler()
 async def sendTOadmin(msg1: types.Message):
     global ban
     if ban == 1:
-        if msg1.text == "stop":
-            ban = 0
-        elif msg1.from_user.id == S:
-            await bot.send_message(userid, msg1.text)
-        else:
-            await bot.send_message(S, msg1.text)
+        uid = msg1.from_user.id
+        await sendtodaun(msg1.text, uid)
 
 
 # @dp.message_handler()
